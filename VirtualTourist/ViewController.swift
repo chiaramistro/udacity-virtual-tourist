f23e1dc54404ea17e429818c2a8ac2b8a612cbd5
Chiara Mistrorigo <c.mistrorigo@reply.it>
2024-03-25 17:41:43 +0100

    Init project

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Chiara Mistrorigo on 25/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
