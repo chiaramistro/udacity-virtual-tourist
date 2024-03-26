8ddf518f7f73be22e2a0093f3d6b0900c28f9cc6
Chiara Mistrorigo <c.mistrorigo@reply.it>
2024-03-26 09:14:00 +0100

    Add mapview and handle long tap through gesture recognizer

--- VirtualTourist/Controllers/TravelLocationsMapViewController.swift
+++ VirtualTourist/Controllers/TravelLocationsMapViewController.swift
@@ -6,17 +6,42 @@
 //
 
 import UIKit
+import MapKit
 
-class TravelLocationsMapViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("TravelLocationsMapViewController viewDidLoad()")
+        
+        mapView.delegate = self
+        initializeGestureRecognizer()
     }
     
+    func initializeGestureRecognizer() {
+        print("initializeGestureRecognizer()")
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleMapTap(gestureRecognizer:)))
+        
+        gestureRecognizer.delegate = self
+        mapView.addGestureRecognizer(gestureRecognizer)
+    }
     
-    @IBAction func goToPhotoAlbum(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "showPhotoAlbum", sender: nil)
+    @objc func handleMapTap(gestureRecognizer: UILongPressGestureRecognizer) {
+        print("handleMapTap()")
+        
+        let location: CGPoint = gestureRecognizer.location(in: mapView)
+                    
+        let coordinates: CLLocationCoordinate2D = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        let annotation : MKPointAnnotation = MKPointAnnotation()
+        annotation.coordinate = coordinates
+        mapView.addAnnotation(annotation)
+        
+        print("annotation \(annotation)")
+        let region = MKCoordinateRegion(center: coordinates, span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+        mapView.setRegion(region, animated: true)
     }
 
 }
