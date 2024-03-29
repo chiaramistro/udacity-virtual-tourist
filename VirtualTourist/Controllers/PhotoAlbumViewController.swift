238a2d5176915453d403be83ce136e3bdbf49d3a
Chiara Mistrorigo <c.mistrorigo@reply.it>
2024-03-29 09:29:16 +0100

    Fix fetching of photos with empty state and loading

--- VirtualTourist/Controllers/PhotoAlbumViewController.swift
+++ VirtualTourist/Controllers/PhotoAlbumViewController.swift
@@ -15,12 +15,15 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, NSFetchedRe
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    @IBOutlet weak var noImagesText: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     var pin: Pin!
+    var photos: [Photo] = []
     
     var dataController: DataController!
     var fetchedResultsController: NSFetchedResultsController<Photo>!
     
-    var numOfPhotos: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -79,35 +82,58 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, NSFetchedRe
     
     func loadPhotos() {
         print("loadPhotos()")
+        toggleLoading(loading: true)
+        noImagesText.isHidden = true
         
-        if let photos = fetchedResultsController.fetchedObjects {
-            if (photos.isEmpty) {
-                // try to load some photos
+        if let fetchedPhotos = fetchedResultsController.fetchedObjects {
+            if (fetchedPhotos.isEmpty) {
+                print("Pin DOES NOT have a photo album")
+                // pin doesn't have a photoalbum yet, fetch photos
                 FlickrClient.getPhotosOnLocation(lat: pin.latitude, lon: pin.longitude) { result, error in
                     print("getPhotosOnLocation() result \(String(describing: result))")
                     // if result is still empty, then show empty state
                     if let result = result {
                         if (result.pages > 0) {
-                            self.fetchPhotoSources(photos: result.photo)
+                            self.fetchPhotoSources(singlePhotos: result.photo)
                         } else {
                             print("Empty state")
+                            self.showEmptyState()
+                            self.toggleLoading(loading: false)
                         }
                     } else {
                         // Error empty state
                         print("Error occurred during getPhotosOnLocation: \(error?.localizedDescription)")
+                        self.showEmptyState()
+                        self.toggleLoading(loading: false)
                     }
                 }
             } else {
-                numOfPhotos = photos.count
+                print("Pin DOES have a photo album")
+                print("Total number of photos \(fetchedPhotos.count)")
+                photos = fetchedPhotos
+                toggleLoading(loading: false)
             }
         }
     }
     
-    func fetchPhotoSources(photos: [SinglePhoto]) {
+    func toggleLoading(loading: Bool) {
+        print("toggleLoading(\(loading))")
+        collectionView.isHidden = loading
+        loading ? activityIndicator.startAnimating() : activityIndicator.stopAnimating()
+    }
+    
+    func showEmptyState() {
+        print("showEmptyState()")
+        photos = [] // reset
+        noImagesText.isHidden = false
+    }
+    
+    func fetchPhotoSources(singlePhotos: [SinglePhoto]) {
         print("fetchPhotoSources()")
-        numOfPhotos = photos.count
+        var loadedPhotos: Int = 0
+        print("Total number of photos \(singlePhotos.count)")
         
-        for photo in photos {
+        for photo in singlePhotos {
             FlickrClient.getPhotoSize(id: photo.id) { result, error in
                 print("getPhotoSize() result \(String(describing: result))")
                 if let result = result {
@@ -118,12 +144,18 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, NSFetchedRe
                         fetchedPhoto.pin = self.pin
                         try? self.dataController.viewContext.save()
                         DispatchQueue.main.async {
-                            self.collectionView!.reloadData()
+                            loadedPhotos+=1
+                            self.photos.append(fetchedPhoto)
+                            if (loadedPhotos == singlePhotos.count) { // finished loading all photos
+                                self.toggleLoading(loading: false)
+                                self.collectionView!.reloadData()
+                            }
                         }
                     }
                 } else {
-                    // Error
+                    // Error, will show placeholder on image
                     print("Error occurred during getPhotoSize: \(error?.localizedDescription)")
+                    loadedPhotos+=1
                 }
             }
         }
@@ -137,36 +169,48 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, NSFetchedRe
 extension PhotoAlbumViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return numOfPhotos
+        return photos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        print("collectionView cell \(indexPath)")
+        print("collectionView cell \(indexPath.row)")
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoAlbumCollectionViewCell", for: indexPath) as! PhotoAlbumCollectionViewCell
         
         let downloadedImage = UIImage(named: "image-placeholder")
         cell.image.image = downloadedImage
         
-        let thePhoto = fetchedResultsController.object(at: indexPath)
+        let thePhoto = photos[indexPath.row]
         
-        if let photoUrl = thePhoto.source {
-            FlickrClient.getImage(url: photoUrl) { photo, error in
-                DispatchQueue.main.async {
-                    if let photo = photo {
-                        let image = UIImage(data: photo)
-                        cell.image.image = image
-                    } else {
-                        // Some error occurred, show placeholder
-                        print("Error occurred during getImage: \(error?.localizedDescription)")
-                        let downloadedImage = UIImage(named: "image-placeholder")
-                        cell.image.image = downloadedImage
+        if let photoData = thePhoto.image {
+            print("Photo DOES have data")
+            // image has data saved in storage, take that
+            let image = UIImage(data: photoData)
+            cell.image.image = image
+        } else {
+            print("Photo DOES NOT have data, check for source")
+            if let photoUrl = thePhoto.source {
+                print("Photo DOES have source, download image")
+                FlickrClient.getImage(url: photoUrl) { photo, error in
+                    print("getImage() result \(String(describing: photo))")
+                    DispatchQueue.main.async {
+                        if let photo = photo {
+                            print("getImage() successful")
+                            thePhoto.image = photo
+                            let image = UIImage(data: photo)
+                            cell.image.image = image
+                        } else {
+                            // Some error occurred, show placeholder
+                            print("Error occurred during getImage: \(error?.localizedDescription)")
+                            let downloadedImage = UIImage(named: "image-placeholder")
+                            cell.image.image = downloadedImage
+                        }
                     }
                 }
+            } else {
+                print("Photo DOES NOT have source")
+                let downloadedImage = UIImage(named: "image-placeholder")
+                cell.image.image = downloadedImage
             }
-        } else {
-            print("Error occurred with photo source")
-            let downloadedImage = UIImage(named: "image-placeholder")
-            cell.image.image = downloadedImage
         }
         
         return cell
