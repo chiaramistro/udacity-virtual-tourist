79aae3ce16642dd78e06e07fe2251813c1ad7b17
Chiara Mistrorigo <c.mistrorigo@reply.it>
2024-04-02 12:28:05 +0200

    Refactor move travel map view controller logic to separate file

---
+++ VirtualTourist/Controllers/TravelLocationsMapViewController+MKMapViewDelegate.swift
@@ -0,0 +1,53 @@
+//
+//  TravelLocationsMapViewController+MKMapViewDelegate.swift
+//  VirtualTourist
+//
+//  Created by Chiara Mistrorigo on 02/04/24.
+//
+
+import UIKit
+import MapKit
+
+extension TravelLocationsMapViewController: MKMapViewDelegate {
+    
+    // MARK: - Map view delegate methods
+    
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        print("mapView regionDidChangeAnimated()")
+        let regionLat: Double = mapView.region.center.latitude
+        let regionLon: Double = mapView.region.center.longitude
+        let zoom: MKCoordinateSpan = mapView.region.span
+        UserDefaults.standard.set(regionLat, forKey: "mapRegionLat")
+        UserDefaults.standard.set(regionLon, forKey: "mapRegionLon")
+        UserDefaults.standard.set(zoom.latitudeDelta, forKey: "mapRegionLatDelta")
+        UserDefaults.standard.set(zoom.longitudeDelta, forKey: "mapRegionLonDelta")
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("mapView didSelect()")
+        let detailedAnnotation = view.annotation as? MKPointAnnotationDetailed
+
+         let photoAlbumController = self.storyboard!.instantiateViewController(withIdentifier: "PhotoAlbumViewController") as! PhotoAlbumViewController
+        photoAlbumController.pin = detailedAnnotation?.pin
+        photoAlbumController.dataController = dataController
+        self.navigationController?.pushViewController(photoAlbumController, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKMarkerAnnotationView
+
+        if pinView == nil {
+            pinView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.glyphTintColor = .white
+            pinView!.markerTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+}
---
+++ VirtualTourist/Model/MKPointAnnotationDetailed.swift
@@ -0,0 +1,14 @@
+//
+//  MKPointAnnotationDetailed.swift
+//  VirtualTourist
+//
+//  Created by Chiara Mistrorigo on 02/04/24.
+//
+
+import Foundation
+import MapKit
+
+// Allow us to connect pin object to an annotation
+class MKPointAnnotationDetailed: MKPointAnnotation {
+    var pin: Pin!
+}
